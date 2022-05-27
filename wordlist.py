00fe523dca73d7918168d4054e7b339de5ca3d94
Phil <103548656+phil401@users.noreply.github.com>
2022-05-26 20:39:13 -0400

    Create wordlist.py

---
+++ wordlist.py
@@ -0,0 +1,6 @@
+words = [
+  
+  'house', 'giant', 'leave', 'pouch', 'honky', 'joint', 'truck', 'shock', 'wheel', 'plant',
+  'plane', 'plain', 'fetch', 'ankle', 'woman', 'elude', 'petty', 'taffy', 'dirty', 'minty',
+  
+  ]
