41b461ab45c6981719576a1361fc847a9f462a45
Phil <103548656+phil401@users.noreply.github.com>
2022-06-02 17:00:14 -0400

    Finalizing Version 1.0
    >> Still working through the logic and learning a TON! 
    >> Added full support for displaying [A] letter in the correct position, (A) letter in the wrong position, and -A- letter that is not in the word. 
    >> Now prints alphabet indicating letters and their respective status.
    >> Open to any constructive input!

--- main.py
+++ main.py
@@ -9,47 +9,54 @@ import random
 import wordlist
 
 secret = random.choice(wordlist.words)
-guesses_remaining = 6
-letters_remaining = string.ascii_lowercase
 
+line = '-'*20
+sline = '*'*20
 
 def guess_word():
 
-  global guesses_remaining
-  while guesses_remaining > 0:
-  
-    guess = input('Guess a 5-letter word:')
-    print('\nGuesses remaining: {}'{guesses_remaining})
-  
-    if len(guess) == 5:
-    
-      if guess == secret:
-        print('You win! The secret word is:\n\n>> {} <<'.format(secret))
-        print('-' * 30)
-        break
-    
-      for i in range(0, len(secret)):
-    
-        if secret[i] == guess[i]:
-          print('[Correct pos]', guess[i]) # i+1 to show index
-          # should actually hide index position, but indicate if it is right or wrong
-  
-        if guess[i] in secret and secret[i] != guess[i]:
-          print('[Wrong pos]', guess[i])
-  
-        if guess[i] not in secret:
-          print('[Not in word' , guess[i])
-          #letters_remaining.replace(secret[i])
-        
-      print('-' * 30)
-  
-    
-      guesses_remaining -= 1
-  
-    elif len(guess) != 5:
-      print('You done messed up')
-  
-  if guesses_remaining == 0:
-    print('Aw man, you lost!\nThe secret word is:\n\n>>{}<<'.format(secret))
+    guesses_remaining = 6
+    letters_remaining = string.ascii_lowercase
+
+    while guesses_remaining != 0:
+        print('\nGuesses remaining: {}'.format(guesses_remaining))
+        print('\nLetters remaining:\n{}\n{}\n'.format(line, letters_remaining))
+        guess = input('Guess a 5-letter word:')
+
+        if len(guess) == 5:
+
+            if guess == secret:
+                print('You win! The secret word is:\n\n>> {} <<'.format(secret))
+                print(line)
+                break
+
+            for i in range(0, len(secret)):
+
+                if guess[i] == secret[i]:
+                    print('+{}+'.format(guess[i])) # i+1 to show index
+                    letters_remaining = letters_remaining.replace(guess[i], '[{}]'.format(guess[i].upper()))
+
+                if guess[i] in secret and secret[i] != guess[i]:
+                    print('({})'.format(guess[i]))
+                    letters_remaining = letters_remaining.replace(guess[i], '({})'.format(guess[i].upper()))
+
+
+                if guess[i] not in secret:
+                    print('-{}-'.format(guess[i]))
+                    #letters_remaining.replace(secret[i])
+                    letters_remaining = letters_remaining.replace(guess[i], '-{}-'.format(guess[i]))
+
+
+
+            print(line)
+
+
+            guesses_remaining -= 1
+
+        elif len(guess) != 5:
+          print('{}\nYou done messed up, honky!\n{}'.format(line, line))
+
+    if guesses_remaining == 0:
+        print('{}\nAw man, you lost!\nThe secret word is:\n\n>>{}<<\n{}'.format(line, secret, line))
 
 guess_word()
